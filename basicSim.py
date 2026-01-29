baa8328ce3ef1bf6ab93a31e41a2b3646b65a671
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-29 18:05:18 +0100

    Bug in simulation

--- basicSim.py
+++ basicSim.py
@@ -2,24 +2,25 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-mb = 0.3
-l = 0.3
-ms = 0.02
-xs = 0.25
-b = 0.01
+mb = 0.0335
+l = 0.128
+ms = 0.02335
+xs = 0.120
+b = 0.001
 g = 9.81
 
 I = (1/3)*mb*l**2 + ms*xs**2
 
-Fs = 1
+Fs = 0
 
 def deg(t,x):
     dx1 = x[1]
-    dx2 = ((-mb*g*l/2 + ms*g*xs)/I)*np.sin(x[0]) - (b/I)*x[1] + (Fs*xs)/I
+    dx2 = -((mb*g*(l/2) + ms*g*xs)/I)*np.sin(x[0]) - (b/I)*x[1] + (Fs*xs)/I
     return [dx1,dx2]
 
-res = solve_ivp(deg, [0,10], [0,0], rtol=1e-10, atol=1e-10)
+res = solve_ivp(deg, [0,6], [np.pi/4,0], rtol=1e-10, atol=1e-10)
 
+plt.title("Odpowiedź wahadła")
 plt.plot(res.t, res.y[0], label="x1")
 #plt.plot(res.t, res.y[1], label="x2")
 plt.legend()
