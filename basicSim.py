734db175bc8e0473eba11becefc31b2272cf6fab
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-01-29 20:32:08 +0100

    Mastering simulation parameters with reference

--- basicSim.py
+++ basicSim.py
@@ -2,27 +2,46 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
+#
+# Simulation
+#
+
 mb = 0.0335
 l = 0.128
 ms = 0.02335
 xs = 0.120
-b = 0.001
+b = 0.00011
 g = 9.81
 
 I = (1/3)*mb*l**2 + ms*xs**2
+I = I/8.8
 
 Fs = 0
+# Składnik tarcia suchego
+# - (Fc/I)*np.tanh(x[1]/0.01)
+# Fc = 0.0006
 
 def deg(t,x):
     dx1 = x[1]
     dx2 = -((mb*g*(l/2) + ms*g*xs)/I)*np.sin(x[0]) - (b/I)*x[1] + (Fs*xs)/I
     return [dx1,dx2]
 
-res = solve_ivp(deg, [0,6], [np.pi/4,0], rtol=1e-10, atol=1e-10)
+res = solve_ivp(deg, [0,3], [np.pi/4,0], rtol=1e-10, atol=1e-10)
+
+#
+# Reference from real device
+#
+
+angles = np.load("angle_log.npy")
+angles = ((angles[688:]+4) / 360) * 2*np.pi
+step = 0.01
+t = np.arange(0, len(angles)*step, step)
+
 
 plt.title("Odpowiedź wahadła")
-plt.plot(res.t, res.y[0], label="x1")
+plt.plot(res.t, res.y[0], label="Simulation")
 #plt.plot(res.t, res.y[1], label="x2")
+plt.plot(t, angles, "--", label="Reference")
 plt.legend()
 plt.grid()
 plt.show()
